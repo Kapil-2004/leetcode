749e49105645ecf46911cb6a08d9d98c26b85e69
Kapil-2004 <167434631+Kapil-2004@users.noreply.github.com>
2024-10-19 11:33:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (79.48%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.java
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.java
@@ -1,21 +1,10 @@
 class Solution {
     public int maximumScore(int a, int b, int c) {
-        PriorityQueue <Integer> pq = new PriorityQueue <>(Collections.reverseOrder());
-        pq.add(a);
-        pq.add(b);
-        pq.add(c);
-        int ans=0;
-        while(pq.size()>1){
-            int max = pq.poll()-1;
-            int max2 = pq.poll()-1;
-            if(max>0){
-                pq.add(max);
-            }
-            if(max2>0){
-                pq.add(max2);
-            }
-            ans++;
+        int[] arr={a,b,c};
+        Arrays.sort(arr);
+        if(arr[2]>=arr[0]+arr[1]){
+            return arr[0]+arr[1];
         }
-        return ans;
+        return (arr[0]+arr[1]+arr[2])/2;
     }
 }
