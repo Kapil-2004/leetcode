a8623ad0ddfa0a44d6876e1658bff747858336a0
Kapil-2004 <167434631+Kapil-2004@users.noreply.github.com>
2024-10-21 22:29:03 +0530

    Time: 12 ms (14.86%) | Memory: 42.8 MB (57.83%) - LeetSync

--- 3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.java
+++ 3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.java
@@ -1,13 +1,26 @@
 class Solution {
     public int maxGoodNumber(int[] nums) {
-        String [] arr = new String [6];
-        arr[0] = Integer.toBinaryString(nums[0])+Integer.toBinaryString(nums[1])+Integer.toBinaryString(nums[2]);
-        arr[1] = Integer.toBinaryString(nums[0])+Integer.toBinaryString(nums[2])+Integer.toBinaryString(nums[1]);
-        arr[2] = Integer.toBinaryString(nums[1])+Integer.toBinaryString(nums[0])+Integer.toBinaryString(nums[2]);
-        arr[3] = Integer.toBinaryString(nums[1])+Integer.toBinaryString(nums[2])+Integer.toBinaryString(nums[0]);
-        arr[4] = Integer.toBinaryString(nums[2])+Integer.toBinaryString(nums[1])+Integer.toBinaryString(nums[0]);
-        arr[5] = Integer.toBinaryString(nums[2])+Integer.toBinaryString(nums[0])+Integer.toBinaryString(nums[1]);
-        Arrays.sort(arr);
-        return Integer.parseInt(arr[5], 2);
+        // Ensure nums has at least 3 elements
+        if (nums.length < 3) {
+            throw new IllegalArgumentException("Input array must have at least three elements.");
+        }
+
+        // Get binary representations
+        String x = Integer.toBinaryString(nums[0]);
+        String y = Integer.toBinaryString(nums[1]);
+        String z = Integer.toBinaryString(nums[2]);
+
+        // Use StringBuilder to construct concatenated binary strings
+        int max = Integer.MIN_VALUE;
+
+        // Concatenate and convert to integer
+        max = Math.max(max, Integer.parseInt(x + y + z, 2));
+        max = Math.max(max, Integer.parseInt(x + z + y, 2));
+        max = Math.max(max, Integer.parseInt(y + x + z, 2));
+        max = Math.max(max, Integer.parseInt(y + z + x, 2));
+        max = Math.max(max, Integer.parseInt(z + x + y, 2));
+        max = Math.max(max, Integer.parseInt(z + y + x, 2));
+
+        return max;
     }
 }
