303b6715ad63aa9e3d596562f2462bd47d07d0a8
Kapil-2004 <167434631+Kapil-2004@users.noreply.github.com>
2024-11-02 17:26:15 +0530

    Time: 64 ms (90.14%) | Memory: 41.3 MB (95.05%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,18 +1,30 @@
 class Solution {
-    public int rangeSum(int[] nums, int n, int left, int right) {
-        ArrayList <Integer> list = new ArrayList <>();
-        for(int i=0 ; i<n ; i++){
-            int sum=0;
-            for(int j=i ; j<n ; j++){
-                sum += nums[j];
-                list.add(sum);
+   public static int rangeSum(int[] nums, int n, int left, int right) {
+        int mod = (int)1e9+7;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator< int[]>(){
+            public int compare(int a[], int b[]){
+                return a[0] - b[0];
+            }
+        });
+
+        for(int i=0;i<n;i++){
+            pq.offer(new int[] {nums[i],i});
+        }
+        int sum =0;
+        for(int i=0;i<right;i++){
+            int cur[] = pq.poll();
+            if(i >=left-1){
+                sum = (sum + cur[0])%mod;
+            }
+
+            if(cur[1]+1 < n){
+                cur[1]++;
+                cur[0]= cur[0] + nums[cur[1]];
+                pq.offer(cur);
             }
         }
-        Collections.sort(list);
-        int ans=0;
-        for(int i=left-1 ; i<right ; i++){
-            ans = (ans+list.get(i))%1_000_000_007;
-        }
-        return ans;
+
+        return sum;
+
     }
 }
