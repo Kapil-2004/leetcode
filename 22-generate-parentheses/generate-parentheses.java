689604a0b22d1f2723d025e217c1a9689e4f18d0
Kapil-2004 <167434631+Kapil-2004@users.noreply.github.com>
2025-04-01 11:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (88.81%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,20 +1,22 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        
         List<String> ans = new ArrayList <>();
-    gen(ans, "", 0, 0, n);
-    return ans;
+        StringBuilder sb = new StringBuilder();
+        path(sb , ans , 0 , 0 , n);
+        return ans;
     }
-    public void gen(List<String> ans, String s, int o, int c, int n){
+    public void path( StringBuilder sb ,List<String> ans, int o, int c, int n){
         if(o==n && c==n){
-            ans.add(s);
+            ans.add(sb.toString());
             return;
         }
         if(o<n){
-            gen(ans, s+'(', o+1, c, n);
+            path( sb.append('(') , ans, o+1, c, n);
+            sb.deleteCharAt(sb.length()-1);
         }
         if(c<o){
-            gen(ans, s+')', o, c+1, n);
+            path(sb.append(')') , ans, o, c+1, n);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
