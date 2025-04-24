450a6ae35b3f48c6e8ca135caef3680b8d5057c4
Kapil-2004 <167434631+Kapil-2004@users.noreply.github.com>
2025-04-24 17:58:46 +0530

    Time: 22 ms (32.80%) | Memory: 53.3 MB (6.79%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,31 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return path(nums,goal) - path(nums , goal-1);
-    }
-    public int path(int []nums , int goal){
-        int sum=0 ;
-        int i=0 ;
+        // return path(nums,goal) - path(nums , goal-1);
+        int sum=0;
         int ans=0;
-        for(int j=0 ; j<nums.length ; j++){
-            sum += nums[j];
-            while(sum>goal && i<=j){
-                sum -= nums[i];
-                i++;
+        HashMap <Integer , Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i=0 ; i<nums.length ; i++){
+            sum += nums[i];
+            if(map.containsKey(sum - goal)){
+                ans += map.get(sum-goal);
             }
-            ans += j-i+1;
-        } 
+            map.put(sum , map.getOrDefault(sum , 0)+1);
+        }
         return ans;
     }
+    // public int path(int []nums , int goal){
+    //     int sum=0 ;
+    //     int i=0 ;
+    //     int ans=0;
+    //     for(int j=0 ; j<nums.length ; j++){
+    //         sum += nums[j];
+    //         while(sum>goal && i<=j){
+    //             sum -= nums[i];
+    //             i++;
+    //         }
+    //         ans += j-i+1;
+    //     } 
+    //     return ans;
+    // }
 }
