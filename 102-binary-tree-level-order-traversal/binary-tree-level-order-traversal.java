de7124798f5fdba542870d51f6a0606cb1d61af5
Kapil-2004 <167434631+Kapil-2004@users.noreply.github.com>
2025-07-06 21:14:34 +0530

    Time: 2 ms (13.31%) | Memory: 45.2 MB (21.74%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,30 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 import java.util.*;
 
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) {
-            return ans;
-        }
         Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> ll = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode a = q.poll();
-                ll.add(a.val);
-                if (a.left != null) {
-                    q.add(a.left);
-                }
-                if (a.right != null) {
-                    q.add(a.right);
-                }
-            }
-            ans.add(ll);
-        }
-
+        traversal(root , ans, q);
         return ans;
+    }
+
+    public static void traversal(TreeNode root , List<List<Integer>> ans , Queue<TreeNode> q ){
+        if(root == null) return ;
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0 ; i<size ; i++){
+                TreeNode node = q.poll();
+                list.add(node.val); 
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            ans.add(new ArrayList<>(list));
+        }
     }
 }
