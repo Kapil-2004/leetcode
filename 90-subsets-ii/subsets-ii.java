8798de119accb720a37fc3978722bd3f5501a0dc
Kapil-2004 <167434631+Kapil-2004@users.noreply.github.com>
2025-07-14 11:11:51 +0530

    Time: 1 ms (99.93%) | Memory: 43.4 MB (78.25%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+
+        path(nums , ans , list , 0);
+        return ans;
+
+    }
+
+    public void path(int []nums , List<List<Integer>>ans , List<Integer> list , int st){
+        ans.add(new ArrayList<>(list));
+        if(st >= nums.length ) return ;
+
+        for(int i=st ; i<nums.length ; i++){
+            if (i > st && nums[i] == nums[i - 1]) continue;
+            list.add(nums[i]);
+            path(nums , ans , list , i+1);
+            list.remove(list.size()-1);
+        }
+    }
+}
